c9fab81670b74abff79e56aa2737c8ba19dba843
GGasset <gassetgerman@gmail.com>
2024-11-20 17:22:41 +0100

    added file + updated headers

--- dealloc.c
+++ dealloc.c
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin_free.c                                  :+:      :+:    :+:   */
+/*   dealloc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ggasset- <ggasset-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 16:07:06 by germangasse       #+#    #+#             */
-/*   Updated: 2024/11/20 17:21:26 by ggasset-         ###   ########.fr       */
+/*   Created: 2024/11/20 17:19:25 by germangasse       #+#    #+#             */
+/*   Updated: 2024/11/20 17:21:58 by ggasset-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "pipex.h"
 
-// Perfect to append file contents to a buffer
-char	*ft_strjoin_free(char const *s1, char const *s2, int free1, int free2)
-{
-	char	*out;
-	
-	out = ft_strjoin(s1, s2);
-	if (s1 && !s2)
-		out = ft_strdup(s1);
-	if (!s1 && s2)
-		out = ft_strdup(s2);
-	if (s1 && free1)
-		free(s1);
-	if (s2 && free2)
-		free(s2);
-	return (out);
-}
